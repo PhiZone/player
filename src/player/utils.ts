cacfcc018f4a06fa8ee3d1bb7d3d08a76f7ac043
Naptie <naptie@qq.com>
2025-01-17 23:01:55 +0800

    refactor: mobile deep linking

--- src/player/utils.ts
+++ src/player/utils.ts
@@ -93,6 +93,33 @@ const easingFunctions: ((x: number) => number)[] = [
 
 export const IS_TAURI = '__TAURI_INTERNALS__' in window;
 
+export const IS_IOS = (() => {
+  const iosQuirkPresent = function () {
+    const audio = new Audio();
+
+    audio.volume = 0.5;
+    return audio.volume === 1; // volume cannot be changed from "1" on iOS 12 and below
+  };
+
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isAppleDevice = navigator.userAgent.includes('Macintosh');
+  const isTouchScreen = navigator.maxTouchPoints >= 1; // true for iOS 13 (and hopefully beyond)
+
+  return isIOS || (isAppleDevice && (isTouchScreen || iosQuirkPresent()));
+})();
+
+export const IS_ANDROID_OR_IOS =
+  IS_IOS ||
+  (() => {
+    if (/windows phone/i.test(navigator.userAgent)) {
+      return false;
+    }
+    if (/android/i.test(navigator.userAgent)) {
+      return true;
+    }
+    return false;
+  })();
+
 const download = async (url: string, name?: string) => {
   EventBus.emit('loading', 0);
   EventBus.emit(
