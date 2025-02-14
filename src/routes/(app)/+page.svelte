1373e176327bcd2e5a5c92a87ba3e82b15fea6bf
Naptie <naptie@qq.com>
2025-02-15 03:23:33 +0800

    refactor: remove unused imports

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import JSZip, { file } from 'jszip';
+  import JSZip from 'jszip';
   import mime from 'mime/lite';
   import { onMount } from 'svelte';
   import queryString from 'query-string';
@@ -9,7 +9,6 @@
     Config,
     IncomingMessage,
     Metadata,
-    OutgoingMessage,
     Preferences,
     RecorderOptions,
     Release,
@@ -45,7 +44,7 @@
   import { tempDir } from '@tauri-apps/api/path';
   import { download as tauriDownload } from '@tauri-apps/plugin-upload';
   import { readFile, remove } from '@tauri-apps/plugin-fs';
-  import { random, re } from 'mathjs';
+  import { random } from 'mathjs';
   import { base } from '$app/paths';
   import { listen } from '@tauri-apps/api/event';
   import { invoke } from '@tauri-apps/api/core';
