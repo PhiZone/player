9e7b843103fcf75ed009e12cf9ccf2db35752fe0
Naptie <naptie@qq.com>
2025-02-23 19:31:20 +0800

    refactor: make .env optional

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,21 @@
+import * as env from '$env/static/public';
 import { sequence } from '@sveltejs/kit/hooks';
 import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
-import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 
-Sentry.init({
-  dsn: PUBLIC_SENTRY_DSN,
+if ('PUBLIC_SENTRY_DSN' in env) {
+  Sentry.init({
+    dsn: env.PUBLIC_SENTRY_DSN,
 
-  tracesSampleRate: 1.0,
+    tracesSampleRate: 1.0,
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  spotlight: import.meta.env.DEV,
-});
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    spotlight: import.meta.env.DEV,
+  });
+}
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
+export const handle = 'PUBLIC_SENTRY_DSN' in env ? sequence(sentryHandle()) : () => {};
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+export const handleError = 'PUBLIC_SENTRY_DSN' in env ? handleErrorWithSentry() : () => {};
