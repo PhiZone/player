101ac0699a834c821763143962f81cbe99be2f5a
Naptie <naptie@qq.com>
2025-03-03 10:07:19 +0800

    build: bump to 0.1.3

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
-export const VERSION = '0.1.2';
+export const VERSION = '0.1.3';
 export const REPO_LINK = 'https://github.com/PhiZone/player';
 export const REPO_API_LINK = 'https://api.github.com/repos/PhiZone/player';
